a2bfab627bed4bbde12e8f722c4b65b7966ae4aa
matias13588 <matias.21@gmail.com>
2023-09-23 20:09:53 +0200

    fix: Deleted redundant comments.

--- number_guess.sh
+++ number_guess.sh
@@ -1,11 +1,12 @@
+# Shell script for a number guessing game.
+# Final project for freeCodeCamp's Relational Database Certification.
+
 #!/bin/bash
 
 PSQL="psql --username=freecodecamp --dbname=number_guess -t --no-align -c"
 
-# generate random number between 1 and 1000
 RANDOM_NUMBER=$(( $RANDOM % 1000 + 1 ))
 
-# ask for username
 echo "Enter your username:"
 read USERNAME
 
@@ -15,17 +16,13 @@ GET_USER_DATA=$($PSQL "
     WHERE username = '$USERNAME'
 ")
 
-# if username is new
 if [[ -z $GET_USER_DATA ]]
 then
-    # save into DB
     SAVE_NEW_USER=$($PSQL "
         INSERT INTO users(username, games_played, best_game) 
         VALUES('$USERNAME', 0, 1000)
     ")
     echo "Welcome, $USERNAME! It looks like this is your first time here."
-
-# if username has been used before
 else
     echo "$GET_USER_DATA" | while IFS=" |" read USERNAME GAMES_PLAYED BEST_GAME
     do
@@ -37,26 +34,23 @@ GUESS_COUNT=0
 
 echo "Guess the secret number between 1 and 1000:"
 
-# ask user to guess number
 GUESS_NUMBER() {
     let GUESS_COUNT++
     read USER_GUESS
 
-    # if user input is not an integer
+    # if the user's input is not an integer
     if ! [[ $USER_GUESS =~ ^[0-9]+$ ]]
     then
         echo "That is not an integer, guess again:"
         GUESS_NUMBER
     fi
 
-    # if user input is lower
     if [[ $USER_GUESS -lt $RANDOM_NUMBER ]]
     then
         echo "It's higher than that, guess again:"
         GUESS_NUMBER
     fi
 
-    # if user input is higher 
     if [[ $USER_GUESS -gt $RANDOM_NUMBER ]]
     then
         echo "It's lower than that, guess again:"
@@ -66,7 +60,6 @@ GUESS_NUMBER() {
 
 GUESS_NUMBER
 
-# if user input is correct
 if [[ $USER_GUESS -eq $RANDOM_NUMBER ]]
 then
     GET_USER_DATA=$($PSQL "
@@ -76,10 +69,8 @@ then
     ")
     echo "$GET_USER_DATA" | while IFS=" |" read BEST_GAME
     do
-        # if number of guesses is lower than the best game
         if [[ $GUESS_COUNT -lt $BEST_GAME ]]
         then
-            # save new best game and game count
             NEW_BEST_GAME=$($PSQL "
                 UPDATE users 
                 SET best_game=$GUESS_COUNT, 
@@ -93,5 +84,6 @@ then
             ")
         fi
     done
+
     echo "You guessed it in $GUESS_COUNT tries. The secret number was $RANDOM_NUMBER. Nice job!"
 fi
